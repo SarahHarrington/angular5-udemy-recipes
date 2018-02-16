93e1e47c34a11bcbecfbac6e4d0d3c11608e08b5
Sarah Harrington <sarah.soberg@gmail.com>
2018-02-16 14:35:55 -0600

    base project set up

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { Recipe } from '../recipes.model';
+
+@Component ({
+    selector: 'app-recipes-list',
+    templateUrl: 'recipes-list.component.html'
+})
+
+export class RecipesListComponent {
+    recipes: Recipe[] = [
+        new Recipe('Test Recipe', 'Test Description', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Recipe-575434.svg/1199px-Recipe-575434.svg.png')
+    ];
+}
