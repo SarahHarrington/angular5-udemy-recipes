aff1c97b0b2ed50c9250ad872e2827dd34150e31
Sarah Harrington <sarah.soberg@gmail.com>
2018-02-16 17:02:35 -0600

    updates to recipes

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -9,6 +9,8 @@ import { Recipe } from '../recipes.model';
 
 export class RecipesListComponent {
     recipes: Recipe[] = [
+        new Recipe('Test Recipe', 'Test Description', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Recipe-575434.svg/1199px-Recipe-575434.svg.png'),
         new Recipe('Test Recipe', 'Test Description', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Recipe-575434.svg/1199px-Recipe-575434.svg.png')
+
     ];
 }
